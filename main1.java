eb4f05e1e49785edc53668eeb300ecf1248779c7
Saidhon4ik <saidkhonabdullaev@gmail.com>
2026-04-12 00:33:38 +0500

    trying to finish that video from Bro code on java

--- main1.java
+++ main1.java
@@ -1,6 +1,30 @@
+//rock  paper scissors
+import java.util.Scanner;
+import java.util.Random;
 public class main1{
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String choices[] = {"rock","paper","scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+        System.out.print("Enter your move(rock, paper, scissors): ");
+        playerChoice = scanner.nextLine().toLowerCase();
+
+        if(!playerChoice.equals("rock") &&
+         !playerChoice.equals("paper") &&
+         !playerChoice.equals("scissors")){
+            System.out.print("Invalid choice!");
+        }
+
+
+        computerChoice = choices[random.nextInt(0,3)];
+        System.out.printf("Computer chooses: %s", computerChoice);
+
+        scanner.close(); 
     }
 }
