f5385edc8775b5edf2993a440f320cad3350f403
Saidhon4ik <saidkhonabdullaev@gmail.com>
2026-04-13 17:39:50 +0500

    it is also a class for main1.java

--- Car.java
+++ Car.java
@@ -1,10 +1,27 @@
+// public class Car{
+//     String make = "Ford";
+//     String model = "Mustang";
+//     int year = 2025;
+//     double price = 58000.99;
+//     boolean isRunning = false;    
 
+//     void start()
+//     {
+//         System.out.print("You start the engine");
+//         isRunning = true;
+//     }
+//     void stop()
+//     {
+//         System.out.println("You stop the engine");
+//         isRunning = false;
+//     }
 
-public class Car{
-    String make = "Ford";
-    String model = "Mustang";
-    int year = 2025;
-    double price = 58000.99;
-    boolean isRunning = false;    
+//     void drive(){
+//         System.out.println("You drive the " + model);
+//     }
 
-}
+//     void brake()
+//     {
+//         System.out.println("You break the " + model);
+//     }
+// }
